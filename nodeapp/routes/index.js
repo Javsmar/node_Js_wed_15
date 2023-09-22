d2d41167428e716c94813577bb302fe40a88f92c
Javier <mastos44@gmail.com>
2023-09-23 00:29:16 +0200

    res, html

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -3,6 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  res.locals.texto = 'Hola';
+  res.locals.nombre = '<script>alert("Inyección de Código")</script>';
+
+  const ahora = new Date();
+  res.locals.esPar = (ahora.getSeconds() % 2) === 0;
+  res.locals.segundoActual = ahora.getSeconds();
+
   res.render('index');
   //res.render('index', { title: 'Express' });
 });
