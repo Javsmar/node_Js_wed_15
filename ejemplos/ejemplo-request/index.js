f7260e8a18ff42057602010fa18a511ade1cc930
Javier <mastos44@gmail.com>
2023-09-28 18:31:31 +0200

    ejemplos

---
+++ ejemplos/ejemplo-request/index.js
@@ -0,0 +1,20 @@
+'use sctrict';
+
+const axios = require('axios');
+
+main().catch(err => console.log('Hubo un error', err));
+
+async function main(){
+
+    //const response = await axios.get('https://swapi.dev/api/people?page=2');
+    const response = await axios.get('http://127.0.0.1:3000/api/agentes');
+    console.log(response.data);
+
+}
+
+
+
+
+
+
+
